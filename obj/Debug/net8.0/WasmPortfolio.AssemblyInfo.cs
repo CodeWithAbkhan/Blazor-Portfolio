7c356e19330c5a0e67e6adb29d64e2210b4aa936
codewithabkhan <itinteractiveacademy@gmail.com>
2024-04-16 11:49:31 +0500

    proj file fixes 3

--- obj/Debug/net8.0/WasmPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/WasmPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WasmPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3573a5594fc9597300d0aeaf85e0b7aa1ee47ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33113347e063f345fa00f3851fcbbec7ab781008")]
 [assembly: System.Reflection.AssemblyProductAttribute("WasmPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WasmPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
