2c36b7b8492882fe42210c2340931ef9751b1d6e
codewithabkhan <itinteractiveacademy@gmail.com>
2024-04-16 14:02:45 +0500

    image src fixes

--- obj/Debug/net8.0/WasmPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/WasmPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WasmPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c5afe4cafa0629067cbba8682a798c8d63201e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1e40e09b56b299885cfa60291b802b7f51ec2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WasmPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WasmPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
